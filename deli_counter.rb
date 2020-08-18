1fe1eff7570c6e8dd4243c570869387506ffb7ba
Alma Flores <flores.almalee@gmail.com>
2020-08-18 22:24:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,11 +2,16 @@
 katz_deli = []
 
 def line(katz_deli)
+  array = []
+  place = 1
   if katz_deli.size == 0
     puts "The line is currently empty."
   else 
-    katz_deli
-    puts "The line is currently: #{katz_deli}"
+    katz_deli.each do |name|
+      array << "#{place}. #{name}"
+      place += 1 
+    end
+    puts "The line is currently: #{array.join(' ')}"
   end
 end
 
